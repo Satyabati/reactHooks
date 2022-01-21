19f10d37f146fcb28ca3c3952408ff123de24c28
Satyabati <satya7994@gmail>
2022-01-21 19:52:24 +0530

    initial commit

--- src/Component/noteapp.js
+++ src/Component/noteapp.js
@@ -6,9 +6,7 @@ import NotesContext from '../context/notes-context';
 const NoteApp = () =>{
     const [notes, dispatch] = useReducer(notesReducer ,[]);
    // const [notes, setNotes] = useState([] );
-    const [title, setTitle] = useState('');
-    const [body,setBody] = useState('');
-  
+
     useEffect(() => {
       console.log('use Effectshould be called once');
       const notes = JSON.parse(localStorage.getItem('notes')); 
@@ -24,16 +22,6 @@ const NoteApp = () =>{
       localStorage.setItem('notes',json)
     },[notes]);
     
-    const addNote = (e)=>{
-      e.preventDefault()
-      dispatch({
-        type:'ADD_NOTE',
-        title,
-        body
-      })
-      setTitle('');
-      setBody('');
-    }
     return (
       <NotesContext.Provider value={{notes,dispatch}}>
       <h1>Notes</h1>
